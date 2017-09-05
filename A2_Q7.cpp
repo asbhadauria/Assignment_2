cb4beba3e8a3ef7011b59bf56998453682296c08
asbhadauria <31289012+asbhadauria@users.noreply.github.com>
2017-09-05 15:15:11 +0530

    Update A2_Q7.cpp

--- A2_Q7.cpp
+++ A2_Q7.cpp
@@ -2,12 +2,12 @@
 #include <cmath>
 using namespace std;
 int main(){
-int a1,a2,a3;
-cout<<"Enter First Angle:";
-cin>>a1;
-cout<<"Enter Second Angle:";
-cin>>a2;
-cout<<"Third Angle Equals:"<<(180-(a1+a2));
-return 0;
+  int a1,a2,a3;
+  cout<<"Enter First Angle:";
+  cin>>a1;
+  cout<<"Enter Second Angle:";
+  cin>>a2;
+  cout<<"Third Angle Equals:"<<(180-(a1+a2));
+  return 0;
 }
 
