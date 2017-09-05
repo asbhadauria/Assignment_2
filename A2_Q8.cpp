4ec54129b574cd05747025202733975c77dd8d5a
asbhadauria <31289012+asbhadauria@users.noreply.github.com>
2017-09-05 15:15:40 +0530

    Update A2_Q8.cpp

--- A2_Q8.cpp
+++ A2_Q8.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 using namespace std;
 int main(){
-cout<<"Enter the Side of the Triangle:";
-int a;
-cin>>a;
-cout<<"Area of the Triangle:"<<((1.732*a*a)/4)<<"\n";
-return 0;
+  cout<<"Enter the Side of the Triangle:";
+  int a;
+  cin>>a;
+  cout<<"Area of the Triangle:"<<((1.732*a*a)/4)<<"\n";
+  return 0;
 }
 
