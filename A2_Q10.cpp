886bbc7d861c09093477ad6a756b5515805a8ea4
asbhadauria <31289012+asbhadauria@users.noreply.github.com>
2017-09-05 15:17:05 +0530

    Update A2_Q10.cpp

--- A2_Q10.cpp
+++ A2_Q10.cpp
@@ -1,22 +1,22 @@
 #include <iostream>
 using namespace std;
 int main(){
-int m1,m2,m3,m4,m5;
-float total;
-cout<<"Enter Marks:";
-cin>>m1;
-cout<<"Enter Marks:";
-cin>>m2;
-cout<<"Enter Marks:";
-cin>>m3;
-cout<<"Enter Marks:";
-cin>>m4;
-cout<<"Enter Marks:";
-cin>>m5;
-total=m1+m2+m3+m4+m5;
-cout<<"Total="<<total<<"\n"<<
-"Average="<<(total/5)<<"\n"<<
-"Percentage="<<(total/5)<<"\n"; 
-return 0;
+  int m1,m2,m3,m4,m5;
+  float total;
+  cout<<"Enter Marks:";
+  cin>>m1;  
+  cout<<"Enter Marks:";
+  cin>>m2;
+  cout<<"Enter Marks:"; 
+  cin>>m3;  
+  cout<<"Enter Marks:";
+  cin>>m4;
+  cout<<"Enter Marks:";
+  cin>>m5;
+  total=m1+m2+m3+m4+m5;
+  cout<<"Total="<<total<<"\n"<<
+  "Average="<<(total/5)<<"\n"<< 
+  "Percentage="<<(total/5)<<"\n"; 
+  return 0;
 }
 
