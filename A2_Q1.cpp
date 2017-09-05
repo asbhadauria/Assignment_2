3a0b328b381180e93147ed5a9eae52ce5b73e190
asbhadauria <31289012+asbhadauria@users.noreply.github.com>
2017-09-05 15:07:57 +0530

    Update A2_Q1.cpp

--- A2_Q1.cpp
+++ A2_Q1.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 using namespace std;
 int main(){
-int len;
-cout<<"Enter Length in Centimeters:";
-cin>>len;
-cout<<"Entered Length in Meters:"<<(0.01*len)<<" cms"<<"\n";
-cout<<"Entered length in Kilometers:"<<(0.00001*len)<<" kms"<<"\n";
-return 0;
+  int len;
+  cout<<"Enter Length in Centimeters:";
+  cin>>len;
+  cout<<"Entered Length in Meters:"<<(0.01*len)<<" cms"<<"\n";
+  cout<<"Entered length in Kilometers:"<<(0.00001*len)<<" kms"<<"\n";
+  return 0;
 }
