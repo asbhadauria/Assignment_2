512c5012630b085d02d5c9fa9a7eb42bdd006dc0
asbhadauria <31289012+asbhadauria@users.noreply.github.com>
2017-09-05 15:07:25 +0530

    Update A2_Q5.cpp

--- A2_Q5.cpp
+++ A2_Q5.cpp
@@ -2,11 +2,11 @@
 #include <cmath>
 using namespace std;
 int main(){
-float num,index;
-cout<<"Enter Base:";
-cin>>num;
-cout<<"Enter Index:";
-cin>>index;
-cout<<"Desired Number:"<<pow(num,index);
-return 0;
+  float num,index;
+  cout<<"Enter Base:";
+  cin>>num;
+  cout<<"Enter Index:";
+  cin>>index;
+  cout<<num<<" raised to "<<index<<" equals "<<pow(num,index);
+  return 0;
 }                
